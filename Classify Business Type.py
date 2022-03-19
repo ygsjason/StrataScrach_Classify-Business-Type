8c2d3d01dfdd83102096b4ed887dfc4197dabce2
ygsjason <99283954+ygsjason@users.noreply.github.com>
2022-03-19 16:23:59 -0500

    Update Classify Business Type.py

--- Classify Business Type.py
+++ Classify Business Type.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 # Start writing code
 df = sf_restaurant_health_violations
-df['category']= df['business_name'].apply(lambda x : 'school' if x.lower().find('school') > 0 
-else 'restaurant' if x.lower().find('restaurant') > 0
-else 'cafe' if (x.lower().find('cafe') > 0 or x.lower().find('coffee') > 0 or x.lower().find('café') > 0)
+df['category']= df['business_name'].apply(lambda x : 'school' if x.lower().find('school') >= 0 
+else 'restaurant' if x.lower().find('restaurant') >= 0
+else 'cafe' if (x.lower().find('cafe') >= 0 or x.lower().find('coffee') >= 0 or x.lower().find('café') >= 0)
 else 'other')
 df[['business_name', 'category']]
 
-#df['café'] = df['business_name'].apply(lambda x: 'True' if x.lower().find('café') > 0 else "")
+#df['café'] = df['business_name'].apply(lambda x: 'True' if x.lower().find('café') >= 0 else "")
 
 #df[['business_name', 'category', 'café']]
 #df.query('business_name == "café"')
